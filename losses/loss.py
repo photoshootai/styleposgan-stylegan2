df9303621909a332f4da350bfe7a9434a5fee395
Madhav <msinghal@uwo.ca>
2021-05-24 00:22:14 -0400

    Refactored reconstruction_loss

--- losses/loss.py
+++ losses/loss.py
@@ -2,10 +2,15 @@ from losses.VGGPerceptual import VGG16
 import torch.nn as nn
 from torchvision import models
 
-def get_total_loss():
+def get_total_loss(I_s, I_dash_s, I_t, I_gen):
+    #get_reconstruction_loss()
+    pass
 
+#Helper function to get reconstruction_loss multiple times as used in total_loss
+def get_reconstruction_loss(I_a, I_b):
+    #
+    pass
     
-
 def get_l1_loss(I_gen, I_gt):
     l1_loss = nn.L1Loss()
     return l1_loss(I_gen, I_gt)
@@ -30,5 +35,5 @@ def face_id_loss():
 def style_gan_loss():
     pass
 
-def patch_d_loss():
+def patch_loss():
     pass
