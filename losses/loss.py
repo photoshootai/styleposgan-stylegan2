284cfdee98a7adc09b31d02ee7a9a32b98e34853
Madhav <msinghal@uwo.ca>
2021-05-25 03:29:40 -0400

    Potentially complete loss calculations

--- losses/loss.py
+++ losses/loss.py
@@ -4,20 +4,27 @@ import torch.nn as nn
 from torchvision import models
 from stylegan2 import hinge_loss, gen_hinge_loss
 
-
 #Facenet
 #TODO: evaluate other options along 2 dimensions of trade off, prediction quality and inference time
 from facenet_pytorch import MTCNN, InceptionResnetV1
 
-def get_total_loss(generated, real, G, D, D_aug, loss_weights, args):
-    total_loss = get_reconstruction_loss() + get_reconstruction_loss() + 
-    return 
+
+
+def get_total_loss_minus_gan_losses(I_dash_s, I_s, I_dash_s_to_t, I_t,  loss_weights, args):
+    total_loss = get_reconstruction_loss(I_dash_s, I_s,  loss_weights, args) + \
+                 get_reconstruction_loss(I_dash_s_to_t, I_t,  loss_weights, args) + \
+                 get_patch_loss(I_dash_s_to_t, I_t)
+    return total_loss 
+
 
 #Helper function to get reconstruction_loss multiple times as used in total_loss
-def get_reconstruction_loss(generated, real, G, D, D_aug, loss_weights, args): 
+def get_reconstruction_loss(generated, real,  loss_weights, args): 
     #Eq 3 in paper
-    rec_loss = loss_weights["l1"]*get_l1_loss(generated, real) + 0#gan_loss(generated, real, G, D)
+    rec_loss =  get_l1_loss(generated, real) + \
+                get_perceptual_vgg_loss(generated, real) +\
+                get_face_id_loss(generated, real) 
     return rec_loss
+
     
 #Eq 4 in paper
 def get_l1_loss(I_gen, I_gt):
@@ -40,29 +47,35 @@ def calcaluate_l_vgg(gen_tuples, gt_tuples):
     return total
 
 #Eq 6
-def face_id_loss():
+#TODO
+def get_face_id_loss(generated, real):
     mtcnn = MTCNN()
     resnet = InceptionResnetV1(pretrained='vggface2').eval()
     img_cropped = mtcnn()
     embedding = resnet()
+    return torch.zeros(generated.shape()[0])
 
 
-def get_gan_loss(generated, real, G, D, D_aug, args):
-    d_x = D_aug(real)
-    d_g_z = D_aug(generated)
+# def get_gan_loss(generated, real,  args):
+#     d_x = D_aug(real)
+#     d_g_z = D_aug(generated)
 
-    return torch.log(d_x) + torch.log(d_g_z)
+
+
+
+#     gan_loss = gan_d_loss() + gan_g_loss()
+    #return torch.log(d_x) + torch.log(1 - d_g_z)
 
 """
 Don't know if we need these two below, but this basically define G and D's losses using BCE seperately, following the usual Pytorch tutorial
 """
-def gan_d_loss(generated, real, G, D, D_aug, args={'device': 'cuda'}):
+def gan_d_loss(generated, real, G, D, D_aug, detach=True,  args={'device': 'cuda'}):
 
     #Training Discriminator
     G_requires_reals = False
     criterion = torch.nn.BCELoss()
 
-    fake_output, fake_q_loss = D_aug(generated.clone().detach(), detach = True)
+    fake_output, fake_q_loss = D_aug(generated, detach = detach)
     real_output, real_q_loss = D_aug(real)
 
     batch_size = generated.shape[0]
@@ -73,8 +86,7 @@ def gan_d_loss(generated, real, G, D, D_aug, args={'device': 'cuda'}):
     disc_loss_real = criterion(real_output, real_label)
     disc_loss_fake = criterion(fake_output, fake_label)
 
-    total_disc_loss = disc_loss_real + disc_loss_fake
-    
+    total_disc_loss = disc_loss_real + disc_loss_fake 
     if args["epoch_id"] % 4== 0:
         gp = gradient_penalty(image_batch, real_output)
         last_gp_loss = gp.clone().detach().item()
@@ -84,14 +96,15 @@ def gan_d_loss(generated, real, G, D, D_aug, args={'device': 'cuda'}):
     return total_disc_loss
 
 
-def gan_g_loss(generated, real, G, D, D_aug, args):
+def gan_g_loss(generated, real, G, D, D_aug, args={'device': 'cuda'}):
     criterion = nn.BCELoss()
     fake_output, _ = D_aug(generated)
 
-  
     batch_size = generated.shape[0]
-    real_label = torch.ones((batch_size, 1), device=self.device)
-    g_loss = criterion(fake_output, real_label)
+    real_label = torch.ones((batch_size, 1), device=args['device'])
+    g_loss = criterion(fake_output, real_label) #-1 * log(D(G(z))
+
+    return g_loss
     
     #Experimental features for contrastive loss and top-k training
 
@@ -123,10 +136,9 @@ def gan_g_loss(generated, real, G, D, D_aug, args):
     #     if not torch.isnan(pl_loss):
     #         gen_loss = gen_loss + pl_loss
 
-    return g_loss
 
-def patch_loss():
-    pass
+def get_patch_loss():
+    return 
 
 
 def _disc_loss_function(real, fake):
--- models/StylePoseGAN.py
+++ models/StylePoseGAN.py
@@ -1,3 +1,4 @@
+from losses.loss import gan_d_loss, get_face_id_loss, get_l1_loss, get_perceptual_vgg_loss
 import os
 import torch
 import torch
@@ -53,30 +54,74 @@ class StylePoseGAN(pl.LightningModule):
     # training_step defined the train loop. # It is independent of forward
     def training_step(self, batch, batch_idx):
         
-        #Get optimizers
-        D_opt, G_opt = self.optimizers()
 
-        
-        (S_pose_map, S_texture_map, source_I), (T_pose_map, T_texture_map, target_I) = batch #x, y = batch, so x is  the tuple, and y is the triplet
+        #Weights
+        weight_l1 =1
+        weight_vgg = 1
+        weight_face = 1
+        weight_gan = 1
+
+        #Get optimizers
+        min_opt, max_opt = self.optimizers()
+
+        min_opt.zero_grad()
+        max_opt.zero_grad()
+
+        (S_pose_map, S_texture_map, I_s), (T_pose_map, T_texture_map, I_t) = batch #x, y = batch, so x is  the tuple, and y is the triplet
 
         #PNet
-        S_E = self.PNet(S_pose_map)
-        T_E = self.PNet(T_pose_map)
+        E_s = self.PNet(S_pose_map)
+        E_t = self.PNet(T_pose_map)
 
         #ANet
-        S_z = self.ANet(S_texture_map)
-        T_z = self.ANet(T_texture_map)
+        z_s = self.ANet(S_texture_map)
+        z_t = self.ANet(T_texture_map)
 
 
         input_noise = None #TODO: make it same as in the lucid rains repo
         I_dash_s = self.g_net.G(z_s, input_noise, E_s) #G(E_s, z_s)            
         I_dash_s_to_t = self.g_net.G(z_s, input_noise, E_t)
+        
+        #Need to detach at the top level 
+        rec_loss_1 =  weight_l1 * get_l1_loss(I_dash_s, I_s) + \
+                      weight_vgg * get_perceptual_vgg_loss(I_dash_s, I_s) + \
+                      weight_face * get_face_id_loss(I_dash_s, I_s)
+                                
+        rec_loss_2 =  weight_l1 * get_l1_loss(I_dash_s_to_t ,I_t) + \
+                      weight_vgg * get_perceptual_vgg_loss(I_dash_s_to_t, I_t) + \
+                      weight_face * get_face_id_loss(I_dash_s_to_t, I_t) 
 
-                
+
+        gan_loss_1_g = gan_g_loss(I_dash_s, I_s, self.g_net.G, self.g_net.D, self.g_net.D_aug)
+        gan_loss_2_g = gan_g_loss(I_dash_s_to_t, I_t, self.g_net.G, self.g_net.D, self.g_net.D_aug)
+
+        
+        gan_loss_1_d = gan_d_loss(I_dash_s.clone().detach(), I_s, self.g_net.G, self.g_net.D, self.g_net.D_aug)
+        gan_loss_2_d = gan_d_loss(I_dash_s_to_t.clone().detach(), I_t, self.g_net.G, self.g_net.D, self.g_net.D_aug)
+        
+        total_loss = rec_loss_1 + rec_loss_2 + \
+                     gan_loss_1_g + gan_loss_2_g + \
+                     -gan_loss_1_d + -gan_loss_2_d
+
+        total_loss.backward() #Minimizing everything
+        
         #Backward pass on different losses
-        #Training Discriminator
+        #!IMPORTANT:  minimizing a -> flip grads = maximizing a
+        # total_loss += (-1) * gan_d_loss()
 
-        D_opt.zero_grad()
+        # Now flip the sign to perform gradient ascent for D and DPatch, and potentially G
+        for p in self.g_net.D.parameters():
+            if p.grad is not None:
+               p.grad.data.mul_(-1) 
+        
+        for q in self.d_patch.parameters():
+            if q.grad is not None:
+                q.grad.data.mul_(-1)
+
+        min_opt.step() #This will minimize all gradients for only ANet, PNet and G
+        max_opt.step() #This will maximize all gradients for D and D_patch b/c they are not flipped
+
+        
 
 
         #Opt step for g_opt and d_opt
@@ -106,11 +151,11 @@ class StylePoseGAN(pl.LightningModule):
     def configure_optimizers(self):
         
         # init optimizers
-        G_opt = Adam(self.g_net.G.parameters(), lr=self.g_lr, betas=(0.5, 0.9))
-        D_opt = Adam(self.g_net.D.parameters(), lr=self.d_lr * self.ttur_mult, betas=(0.5, 0.9))
+        # G_opt = Adam(self.g_net.G.parameters(), lr=self.g_lr, betas=(0.5, 0.9))
+        # D_opt = Adam(self.g_net.D.parameters(), lr=self.d_lr * self.ttur_mult, betas=(0.5, 0.9))
 
         param_to_min = list(self.a_net.parameters() + list(self.p_net.parameters()) + list(self.g_net.parameters()))
-        param_to_max = list(self.g_net.D.parameters() + self.d_patch.parameters())
+        param_to_max = list(self.g_net.D.parameters()) + list(self.d_patch.parameters())
         min_opt = Adam(param_to_min, lr=self.main_lr, betas=(0.5, 0.9))
         max_opt = Adam(param_to_max, lr=self.main_lr, betas=(0.5, 0.9))
         
