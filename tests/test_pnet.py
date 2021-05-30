64e6be3572b8cef2ce8a28b99af90e5ffc4bc2b2
Justin <justin.soanes@gmail.com>
2021-05-30 14:45:19 -0400

    Setup pytest

--- tests/test_pnet.py
+++ tests/test_pnet.py
@@ -1,36 +1,39 @@
 import torch
-import torch.nn as nn
-from os import listdir
-from PIL import Image as PImage
-import numpy as np
-#import pytest
-
+#import torchvision
+#import os
+import sys
+#sys.path.insert(0, './models')
 from models import PNet
 
-def loadImages(path):
-    # return array of images
+#batch_size=               1
+#gpus=                     1
+#image_size=               512
+#tpu_cores=                8
+#precision=                16 
+#accumulate_grad_batches=  32
+#top_k_training=           False
+#deterministic=            True
+testtensor = torch.rand(1, 3, 512, 512)
 
-    imagesList = listdir('E:\Test Images\PoseMaps')
-    loadedImages = []
-    for image in imagesList:
-        print(path + image)
-        fullpath = path+image
-        img = PImage.open(fullpath)
-        loadedImages.append(img)
+#from torch.utils.data import Dataset, DataLoader, random_split
+#from datasets import DeepFashionDataset
 
-    return loadedImages
+#source_image_path="/content/drive/MyDrive/TrainingData/SourceImages"
+#pose_map_path="/content/drive/MyDrive/TrainingData/PoseMaps"
+#texture_map_path="/content/drive/MyDrive/TrainingData/TextureMaps"
+
+#dm = DeepFashionDataset(source_image_path, pose_map_path, texture_map_path, True, batch_size)
+#dataloader = DataLoader(dm, batch_size, num_workers=1)
+#batch = next(iter(dataloader))
+
+#print("batch", batch)
+
+def pnet_forward(input):
+  pnet = PNet(im_chan=3)
+  output = pnet(input)
+  return output.size()
 
 
-path = "E:\Test Images\PoseMaps"
-print('hello')
-# your images in an array
-imgs = loadImages(path)
-
-class test_pnet():
-    
-    def pnet_test(input):
-       pnet = PNet(im_chan=3)
-       output = pnet(input)
-       return output.size()
-
-    assert pnet_test(torch.rand(8, 3, 512, 512)) == (8, 512, 16, 16)
+def test_pnet_forward():
+    testtensor= torch.rand(1, 3, 512, 512)
+    assert(pnet_forward(testtensor) == (1, 512, 32, 32))
