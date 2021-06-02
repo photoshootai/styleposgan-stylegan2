c5c9f824eddaf949027fef82b2df5f174814187a
Madhav <msinghal@uwo.ca>
2021-06-02 03:51:32 -0400

    Reverting ANet changes

--- models/ANet.py
+++ models/ANet.py
@@ -1,21 +1,20 @@
 import torch.nn as nn
 from .layers import ResidualBlock
 
-def get_anet_final_block(in_chan=512, out_chan=2048, hidden_chan=1024, kernel_size=(3,3), stride=4, padding=1, bias=False):
+def get_anet_final_block(in_chan=512, out_chan=2048, hidden_chan=1024, kernel_size=(3,3), stride=2, padding=1, bias=False):
 
     return nn.Sequential(
         #the normalization was recommended by kshitij. It wasn't explicitly prescribed in the text
-        nn.Conv2d(in_chan, in_chan, kernel_size, stride, padding),
+        nn.Conv2d(in_chan, hidden_chan, kernel_size, stride, padding),
         nn.ReLU(inplace=True),
-        # nn.Conv2d(hidden_chan,hidden_chan,kernel_size,stride,padding),
-        # nn.ReLU(inplace=True),
-        # nn.Conv2d(hidden_chan,hidden_chan,kernel_size,stride,padding),
-        # nn.ReLU(inplace=True),
-        nn.Conv2d(in_chan,in_chan,kernel_size,stride,padding),
+        nn.Conv2d(hidden_chan,hidden_chan,kernel_size,stride,padding),
         nn.ReLU(inplace=True),
-        nn.Flatten()
-        # nn.Conv2d(hidden_chan,hidden_chan,kernel_size,2,padding),
-        # nn.ReLU(inplace=True)
+        nn.Conv2d(hidden_chan,hidden_chan,kernel_size,stride,padding),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(hidden_chan,hidden_chan,kernel_size,stride,padding),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(hidden_chan,hidden_chan,kernel_size,stride,padding),
+        nn.ReLU(inplace=True)
     )
 
 class ANet(nn.Module):
@@ -27,7 +26,8 @@ class ANet(nn.Module):
         self.res_block3 = ResidualBlock(in_chan=128,out_chan=256)
         self.res_block4 = ResidualBlock(in_chan=256,out_chan=512)
         self.last_block = get_anet_final_block() #just the Conv2d parts
-        self.linear = nn.Linear(2048,2048)
+        self.linear = nn.Linear(1024,2048)
+
     def forward(self, x):
         '''
         Function for completing a forward pass of the ANet: Given a texture map tensor, 
@@ -45,7 +45,7 @@ class ANet(nn.Module):
         x = self.last_block(x)
 
         #to make it compatible with the fully connected layer, move channels to the final index
-        # x = x.permute(0,2,3,1)
+        x = x.permute(0,2,3,1)
         z = self.linear(x)
         z = z.squeeze(dim=1) # remove an empty axis, another axis could optionally be removed
         
