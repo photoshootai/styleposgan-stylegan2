bda3a8d18f19869f7a3d46c8fe4bb3caf293cbdd
Madhav Singhal <msinghal@uwo.ca>
2021-06-25 15:34:49 -0400

    Adding tmp script file to run

---
+++ stylegan2/script.sh
@@ -0,0 +1,25 @@
+#Environment Activate
+
+
+# Upload Dataset
+rsync -P ./DeepFashionWithFace user@
+
+or 
+
+pip install gdown 
+
+gdown "https://drive.google.com/file/d/1R6DnSBPDLxboyHNRkaR6gTtyRE-ZODGn/view?usp=sharing"
+
+
+#Extract Data
+!mkdir './styleposgan-stylegan2/data'
+!unzip -qq '' -d './styleposgan-stylegan2/data'
+
+# Clone 
+!git clone https://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@github.com/photoshootai/styleposgan-stylegan2.git
+!git checkout ddp-fixes
+
+
+
+#Run Training
+!python train.py --data ./data/DeepFashionWithFace --multi-gpus --image-size 256 --batch-size 4
