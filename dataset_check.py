cd9ff2eee9e9accf20eb11319d0cb1aeaaa02b96
Kshitij Shah <kshnik@gmail.com>
2021-06-24 21:41:43 -0400

    Refactored dataset_check.py

--- dataset_check.py
+++ dataset_check.py
@@ -5,21 +5,9 @@ import PIL
 from datasets import DeepFashionDataset
 from torch.utils.data import DataLoader
 
-
-def main():
-    data_path = "./data/DeepFashionWithFace"
-    batch_size = 4
-    num_workers = 1
-    image_size = (512, 512)
-
-    dataset = DeepFashionDataset(data_path, image_size=image_size)
-    dataloader = DataLoader(
-        dataset, batch_size=batch_size, num_workers=num_workers)
-
-    batch = next(iter(dataloader))
-    # x, y = batch, so x is  the tuple, and y is the triplet
+def show_batch(batch):
     (I_s, P_s, A_s), (I_t, P_t) = batch
-    for i in range(batch_size):
+    for i in range(I_s.shape[0]):
         print(I_s[i].size())
         im = transforms.ToPILImage()(I_s[i]).convert("RGB")
         im.show("Source Image")
@@ -41,6 +29,21 @@ def main():
         im.show("Target Pose Map")
 
         input("Press Enter to continue...")
+
+
+def main():
+    data_path = "./data/DeepFashionWithFace"
+    batch_size = 4
+    num_workers = 1
+    image_size = (512, 512)
+
+    dataset = DeepFashionDataset(data_path, image_size=image_size, seed=24) #Change seed to get different images
+    dataloader = DataLoader(
+        dataset, batch_size=batch_size, num_workers=num_workers)
+
+    batch = next(iter(dataloader))
+    # x, y = batch, so x is  the tuple, and y is the triplet
+    show_batch(batch)
 
 
 if __name__ == "__main__":
