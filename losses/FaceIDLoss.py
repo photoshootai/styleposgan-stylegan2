875e34c96206ab144b8eb548a8685eda97de7cc4
Madhav Singhal <msinghal@uwo.ca>
2021-08-04 12:45:59 -0400

    COMMIT for trying face-detection

--- losses/FaceIDLoss.py
+++ losses/FaceIDLoss.py
@@ -1,8 +1,12 @@
+import face_detection as FD
+import numpy as np
 import torch
 import torch.nn as nn
+import torchvision
 from facenet_pytorch import MTCNN, InceptionResnetV1
+from torchvision import transforms
+import cv2
 
-import numpy as np
 
 class FaceIDLoss(nn.Module):
     def __init__(self, mtcnn_crop_size, weight=None, size_average= True, select_largest=True, requires_grad=False, rank=-1):
@@ -13,7 +17,7 @@ class FaceIDLoss(nn.Module):
             self.device = f'cuda:{rank}'
 
         self.mtcnn_crop_size = mtcnn_crop_size
-        self.mtcnn = MTCNN(image_size=mtcnn_crop_size, select_largest=True, device=self.device).eval()
+        self.mtcnn = MTCNN(image_size=mtcnn_crop_size, min_face_size=10, margin=20, select_largest=True, device=self.device).eval()
         self.resnet = InceptionResnetV1(pretrained='vggface2', device=self.device).eval()
 
         if not requires_grad:
@@ -49,39 +53,99 @@ class FaceIDLoss(nn.Module):
         normalize = lambda t: (t - torch.min(t)) / (torch.max(t) - torch.min(t))
         is_valid_face = lambda i, c, p: c[i] is not None and p[i] > 0.95 
         build_face_mask = lambda c, p: [i for i in range(len(c)) if is_valid_face(i, c, p)]
+        extract_crop = lambda t, b: torch.cat([t[i, :, int(y0):int(y1), int(x0):int(x1)] for i, (x0, y0, x1, y1, _) in zip(range(t.shape[0]), b)], dim=0)
 
         generated, real = normalize(generated), normalize(real)
+        # (b, h, w, c) gets expanded to (1, b, h, w, c) then expects shape len 4, wtf
 
-        real_crops, real_probs = self.mtcnn(real.permute(*perm), return_prob=True)
-        gen_crops = self.mtcnn(generated.permute(*perm))
+        det = FD.build_detector('DSFDDetector', confidence_threshold=0.5, nms_iou_threshold=0.3)
+        print(real.permute(*perm).squeeze().cpu().numpy().astype(np.uint8).shape)
+        real_det = det.detect(real.permute(*perm).squeeze().detach().cpu().numpy().astype(np.uint8))
+        gen_det = det.detect(generated.permute(*perm).squeeze().detach().cpu().numpy().astype(np.uint8))
 
-        if not is_batched:
-            real_crops = [real_crops]
-            real_probs = [real_probs]
-            gen_crops = [gen_crops]
 
-        mask = build_face_mask(real_crops, real_probs)
-        has_face_real = [real_crops[i] for i in mask]
-        should_have_face_gen = [gen_crops[i] for i in mask]
-        # print(mask)
+        print(len(real_det), real_det) #nani???
+        print(len(gen_det), gen_det)
+        real_crops = extract_crop(real, real_det)
+        gen_crops = extract_crop(generated, gen_det)
 
-        if not has_face_real:
-            return torch.tensor(0.0, device=self.device)
-        # print(len(has_face_real), len(should_have_face_gen))
+        
 
-        real_crops_with_face = torch.stack(has_face_real)#.to(device)
-        real_embeddings = self.resnet(real_crops_with_face).detach()
+        # torchvision.utils.save_image(real, "./results/debug_real_before_mtcnn.png", nrow=real.shape[0])
+        # torchvision.utils.save_image(generated, "./results/debug_generated_before_mtcnn.png", nrow=real.shape[0])
+
+        # toPIL = transforms.ToPILImage()
+        # real = [toPIL(t) for t in real]
+        # generated =  [toPIL(t) for t in generated]
+
+
+        # print(real.size())
+        
+        # # real.show()
+        # # generated.show()
+
+        # real_crops, real_probs = self.mtcnn(real.permute(*perm), return_prob=True)
+        # gen_crops, gen_probs = self.mtcnn(generated.permute(*perm), return_prob=True)
+
+ 
+        # torchvision.utils.save_image(real_crops, "./results/debug_real_mtcnn_crops.png", nrow=real_crops.shape[0])
+        # torchvision.utils.save_image(gen_crops, "./results/debug_generated_mtcnn_crops.png", nrow=real_crops.shape[0])
+
+        # torchvision.utils.save_image(real_probs, "./results/debug_real_mtcnn_probs.png", nrow=real_probs.shape[0])
+        # torchvision.utils.save_image(gen_probs, "./results/debug_generated_mtcnn_probs.png", nrow=real_probs.shape[0])
+
+        # print(real_crops.shape)
+        # print(gen_crops.shape)
+
+        # print(real_probs.shape)
+        # print(gen_probs.shape)
+
+        # print(real_crops.shape)
+        # print(gen_crops.shape)
+
+        # print(real_probs.shape)
+
+        # print("Real Crops: ", [t.size() for t in real_crops])
+        # print("Gen Crops: ", [t.size() for t in gen_crops])
+
+        # """
+        # Debugging
+        # """
+
+        # torchvision.utils.save_image(real_crops, "./results/debug_real_cropped.png", nrow=len(real))
+        # torchvision.utils.save_image(gen_crops, "./results/debug_generated_cropped.png", nrow=len(real))
+
+        # input("Saved reals and generated to debug files...")
+
+        # if not is_batched:
+        #     real_crops = [real_crops]
+        #     # real_probs = [real_probs]
+        #     gen_crops = [gen_crops]
+
+        # mask = build_face_mask(real_crops, real_probs)
+        # has_face_real = [real_crops[i] for i in mask]
+        # should_have_face_gen = [gen_crops[i] for i in mask]
+        # # print(mask)
+
+        # if not has_face_real:
+        #     # print("Hit return")
+        #     return torch.tensor(0.0, device=self.device)
+        # # print(len(has_face_real), len(should_have_face_gen))
+
+        # real_crops_with_face = torch.stack(real_crops).to(device)
+        real_embeddings = self.resnet(real_crops).detach()
         # print(real_crops_with_face.shape)
         # print(real_embeddings.shape)
 
-        fill_none_in_gen = [(c if c is not None else torch.zeros((3, crop_size, crop_size))) for c in should_have_face_gen]
+        # fill_none_in_gen = [(c if c is not None else torch.zeros((3, crop_size, crop_size))) for c in should_have_face_gen]
         # print([x.shape for x in fill_none_in_gen])
-        gen_crops_with_face = torch.stack(fill_none_in_gen)#.to(device)
-        gen_embeddings = self.resnet(gen_crops_with_face).detach()
+        # gen_crops_with_face = torch.stack(fill_none_in_gen).to(device)
+        gen_embeddings = self.resnet(gen_crops).detach()
 
-        # show_tensor_list = lambda t: torch.hstack([(c.permute(1, 2, 0) if c is not None else torch.zeros((crop_size, crop_size, 3))) for c in t]).cpu().numpy() * 255
-        # cv2_imshow(show_tensor_list(real_crops_with_face))
-        # cv2_imshow(show_tensor_list(gen_crops_with_face))
+        show_tensor_list = lambda t: torch.hstack([(c.permute(1, 2, 0) if c is not None else torch.zeros((crop_size, crop_size, 3))) for c in t]).cpu().numpy() * 255
+        cv2.imshow(show_tensor_list(real_crops))
+        cv2.imshow(show_tensor_list(gen_crops))
 
-        face_loss = nn.L1Loss(gen_embeddings, real_embeddings, reduction='mean')
+
+        face_loss = nn.L1Loss()(gen_embeddings, real_embeddings)
         return face_loss
